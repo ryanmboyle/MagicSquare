ef38cfddc84ef028974a3ab078352fefcc7ebae1
Ryan Boyle <boyler@gfacademy.org>
2020-04-28 10:26:57 -0400

    Initial sharing of project

---
+++ MagicSquare.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class MagicSquare here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MagicSquare
+{
+    
+    private int dimension;
+    private int[][] data;
+
+    
+    public MagicSquare(int size)
+    {
+        
+    }
+
+
+}
